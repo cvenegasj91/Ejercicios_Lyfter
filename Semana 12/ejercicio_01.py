8b97a77cd99a1b50857d2da485fc26061354d872
Carlos Venegas <venegask_91@hotmail.com>
2025-09-15 22:00:48 -0600

    Cambios solicitados del ejercicio_01

--- Semana 12/ejercicio_01.py
+++ Semana 12/ejercicio_01.py
@@ -3,37 +3,59 @@ class BankAccount:
         self.balance = balance
     
     def deposit(self, amount):
-        if amount > 0:
-            self.balance += amount
-            print(f'Deposito de {amount} realizado a su cuenta' + f'\nNuevo saldo de: {self.balance}')
-        else:
-            print("Debe depositar una cifra mayor a cero")
+        if amount <= 0:
+            raise ValueError("El deposito debe ser mayor a 0")
+        self.balance += amount
+        return self.balance
+        
 
     def withdraw(self, amount):
-        if amount <= self.balance:
-            self.balance -= amount
-            print(f'Retiro de {amount} realizado a su cuenta' + f'\nNuevo saldo de {self.balance}')
-        else:
-            print("Saldo insuficiente")
+        if amount <= 0:
+            raise ValueError("El monto a retirar debe ser mayor a 0")
+        if amount > self.balance:
+            raise RuntimeError("Fondos insuficientes")
+        self.balance -= amount
+        return self.balance
+
 
 class SavingsAccount(BankAccount):
     def __init__(self, balance=0, min_balance=0):
+        if min_balance < 0:
+            raise ValueError("El minimo no puede ser menor a 0")
         self.min_balance = min_balance
         super().__init__(balance)
 
     def withdraw(self, amount):
         if self.balance - amount < self.min_balance:
-            print("Error: Saldo por debajo del minimo")
-        else:
-            super().withdraw(amount)
+            raise PermissionError("Saldo por debajo del minimo")
+        return super().withdraw(amount)
 
+try:
+    cuenta1 = BankAccount(100)
+    print(f"Se creo cuenta de corriente con {cuenta1.balance} dolares")
+    cuenta1.deposit(50)
+    print(f'Nuevo saldo despues del deposito {cuenta1.balance} dolares')
+    cuenta1.withdraw(25)
+    print(f'Nuevo saldo despues del retiro {cuenta1.balance} dolares')
+    cuenta1.withdraw(150)
+    print(f'Nuevo saldo despues del retiro {cuenta1.balance} dolares')
+except ValueError as e:
+    print(f'Error: {e}')
+except RuntimeError as e:
+    print(f'Error: {e}')
 
-cuenta1 = BankAccount(100)
-cuenta1.deposit(50)
-cuenta1.withdraw(25)
-cuenta1.withdraw(150)
-
-cuenta2 = SavingsAccount(100, 25)
-cuenta2.deposit(50)
-cuenta2.withdraw(50)
-cuenta2.withdraw(80)
+try:
+    cuenta2 = SavingsAccount(100, 25)
+    print(f'Se creo cuenta de ahorros con {cuenta2.balance} dolores y un minimo de {cuenta2.min_balance} dolares')
+    cuenta2.deposit(50)
+    print(f'Nuevo saldo despues del deposito {cuenta1.balance} dolares')
+    cuenta2.withdraw(50)
+    print(f'Nuevo saldo despues del retiro {cuenta1.balance} dolares')
+    cuenta2.withdraw(80)
+    print(f'Nuevo saldo despues del retiro {cuenta1.balance} dolares')
+except ValueError as e:
+    print(f'Error: {e}')
+except RuntimeError as e:
+    print(f'Error: {e}')
+except PermissionError as e:
+    print(f'Error: {e}')
